a63359a5ef2b162d14c6d61c78f49e866ada06f0
KevinKing66 <kevinvallenato2002@gmail.com>
2021-12-09 15:34:49 -0500

    cambio de puerto

--- eventos/index.js
+++ eventos/index.js
@@ -1,6 +1,6 @@
 var app = require('./app'),
 mongoose = require('./DB/CBD');
 
-app.listen(1001, () =>{console.log("SG")});
-// app.listen(process.env.port || 8080, () =>{console.log("SG")});
+// app.listen(1001, () =>{console.log("SG")});
+app.listen(process.env.port || 1001, () =>{console.log("SG")});
 
