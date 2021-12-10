d106dcfedfd96cedea58a7ea337d5af5fb84623c
KevinKing66 <kevinvallenato2002@gmail.com>
2021-12-10 18:33:08 -0500

    login

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 var app = require('./app'),
-port = 1000,
+port = 1000  || process.env.PORT,
 mongoose = require('./DB/CBD');
-app.listen(port, () =>{console.log("SG")});
+app.listen(port , () =>{console.log("SG")});
 
